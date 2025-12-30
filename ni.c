6e111c6a1e363e5105f9278cf1e02cc6f9f39329
lking001 <409782709@qq.com>
2025-12-30 17:44:24 +0800

    Update ni.c

--- ni.c
+++ ni.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+	
+	printf("Hello world！\n");printf("Hello world！\n");
 	printf("Hello world！\n");
+	printf("Hello world！\n");
+	printf("Hello world！\n");
+	printf("Hello world！\n");
+	printf("Hello world！\n");printf("Hello world！\n");
+	printf("Hello world！\n");printf("Hello world！\n");printf("Hello world！\n");printf("Hello world！\n");vprintf("Hello world！\n");
 	return 0;
 }
